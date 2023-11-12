f69cdd267f273447139b6382a3a41f8d35e21e86
Tiago <tiagobnarita@gmail.com>
2023-11-12 18:23:37 -0300

    finalizando o create acount

--- WindowsFormsApp1/formCreateAccount.cs
+++ WindowsFormsApp1/formCreateAccount.cs
@@ -16,10 +16,32 @@ namespace WindowsFormsApp1
         public frmCreateAccount()
         {
             InitializeComponent();
+        }
 
-            MySqlConnection con = new MySqlConnection(@"server=127.0.0.1;uid=root;pwd=834483Ti;database=bd_lp2;ConnectionTimeout=1");
-            MySqlCommand cmd = new MySqlCommand();
-            MySqlDataAdapter da = new MySqlDataAdapter();
+        MySqlConnection con = new MySqlConnection(@"server=localhost;port=3306;uid=root;pwd=834483Ti;database=barbagenda");
+        MySqlCommand cmd = new MySqlCommand();
+        MySqlDataAdapter da = new MySqlDataAdapter();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "" && txtPassword.Text == "" && txtConfPassword.Text == "")
+            {
+                MessageBox.Show("Usarname and Password fields are empty", "Registration failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if(txtPassword.Text == txtPassword.Text)
+            {
+                con.Open();
+                string register = "INSERT INTO users VALUES ('" + txtUsername.Text + "','" + txtPassword.Text + "')";
+                cmd = new MySqlCommand(register, con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                MessageBox.Show("Your Account has been Successfully created", "Registration Sucess", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Passwords does not match, Please Re-enter", "Registration failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
