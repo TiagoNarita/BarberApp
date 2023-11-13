cd32e7dd1b7ccdb5667d046a9bee14b9037161a2
Tiago <tiagobnarita@gmail.com>
2023-11-13 14:05:56 -0300

    criando createAccount

---
+++ WindowsFormsApp1/formCreateAccount.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmCreateAccount
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmCreateAccount
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(135)))), ((int)(((byte)(140)))), ((int)(((byte)(145)))));
+            this.ClientSize = new System.Drawing.Size(285, 544);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmCreateAccount";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "formCreateAccount";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
